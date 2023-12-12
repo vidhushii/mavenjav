df71cf85047020635dbf4ef5144a6223fdc1b512
vidhushii <vidhushi8434@gmail.com>
2023-12-12 16:09:25 +0530

    maven java push

---
+++ src/test/java/kmit/samplejava/AppTest.java
@@ -0,0 +1,20 @@
+package kmit.samplejava;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
